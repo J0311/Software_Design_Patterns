10011e59488029920d46206700c759ab4bd877be
Joseph Walker <96276860+J0311@users.noreply.github.com>
2022-08-24 14:57:55 -0400

    Update Student.java

--- Enter_The_DAO/src/Java_Main/Student.java
+++ Enter_The_DAO/src/Java_Main/Student.java
@@ -2,7 +2,7 @@ package Java_Main;
 
 // Basic student class with 2 instance variables - a student name and roll number
 // We will use the student roll number variable (rollno) to access our data base
-// table and retrieve the student name
+// table and retrieve the student name.
 
 public class Student {
 
